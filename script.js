11a3d47a2f4727f78eee2a9b407f356bccddffad
youngjerald239 <youngjerald239@gmail.com>
2022-09-02 16:13:24 -0500

    added player Rect to canvas

--- script.js
+++ script.js
@@ -11,19 +11,31 @@ window.addEventListener('load', function(){
                 if ((e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key ==='ArrowRight') && this.keys.indexOf(e.key) === -1) {
                     this.keys.push(e.key)
                 }
-                console.log(e.key, this.keys)
             })
             window.addEventListener('keyup', e => {
                 if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key ==='ArrowRight') {
                     this.keys.splice(this.keys.indexOf(e.key), 1)
                 }
-                console.log(e.key, this.keys)
             })    
         }
     }
 
     class Player {
-
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth
+            this.gameHeight = gameHeight
+            this.width = 200
+            this.height = 200
+            this.x = 0
+            this.y = this.gameHeight - this.height
+        }
+        draw(context){
+            context.fillStyle = 'white'
+            context.fillRect(this.x, this.y, this.width, this.height)
+        }
+        update(){
+            this.x++
+        }
     }
 
     class Background {
@@ -43,6 +55,8 @@ window.addEventListener('load', function(){
     }
 
     const input = new InputHandler()
+    const player = new Player(canvas.width, canvas.height)
+    player.draw(ctx)
 
     function animate(){
 
