1bd025d6339607d6781ed0958ab74b78fefb16a4
youngjerald239 <youngjerald239@gmail.com>
2022-09-02 15:49:50 -0500

    added key inputhandlers

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ window.addEventListener('load', function(){
     class InputHandler {
         constructor(){
             this.keys = []
-            window.addEventListener('keydown', function(e){
-                console.log(e)
+            window.addEventListener('keydown', e => {
+                if ((e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key ==='ArrowRight') && this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key)
+                }
+                console.log(e.key, this.keys)
             })
+            window.addEventListener('keyup', e => {
+                if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key ==='ArrowRight') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1)
+                }
+                console.log(e.key, this.keys)
+            })    
         }
     }
 
