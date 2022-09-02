45cf5f8ec699bfdfde615535ab2bdb3e56a92a09
youngjerald239 <youngjerald239@gmail.com>
2022-09-02 16:35:08 -0500

    set stop boundary for canvas edges

--- script.js
+++ script.js
@@ -43,7 +43,13 @@ window.addEventListener('load', function(){
             this.x += this.speed
             if (input.keys.indexOf('ArrowRight') > -1){
                 this.speed = 5
+            } else if (input.keys.indexOf('ArrowLeft') > -1){
+                this.speed = -5
+            } else {
+                this.speed = 0
             }
+            if (this.x < 0) this.x = 0
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width
         }
     }
 
